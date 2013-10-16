3eed6deca2fee29909c058806e34f1c7665c3a58
Adrian <adrian.george@rackspace.com>
2013-10-16 09:12:42 -0500

    very basic bootstrap of puppet for our needs

---
+++ bootstrap-puppet-client.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -O /tmp/epel-release-5-4.noarch.rpm http://download.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
+sudo rpm -Uvh /tmp/epel-release-5-4.noarch.rpm
+rm -f /tmp/epel-release-5-4.noarch.rpm
+
+sudo yum install -y puppet
+sudo yum install -y git
